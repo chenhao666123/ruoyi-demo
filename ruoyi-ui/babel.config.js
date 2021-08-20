236f4fbca562a6727e42867c4166b61c1f97d57a
Skyenought <Skyenought@github.com>
2021-08-20 12:08:21 +0800

    vue-- 增加layout

--- ruoyi-ui/babel.config.js
+++ ruoyi-ui/babel.config.js
@@ -2,13 +2,19 @@ module.exports = {
   presets: [
     // https://github.com/vuejs/vue-cli/tree/master/packages/@vue/babel-preset-app
     '@vue/cli-plugin-babel/preset',
-    '@vue/babel-preset-jsx'
+    '@vue/babel-preset-jsx',
+    'babel-plugin-syntax-jsx',
+    'env'
   ],
   'env': {
     'development': {
       // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
       // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
-      'plugins': ['dynamic-import-node']
+      'plugins': [
+        'dynamic-import-node',
+        '@babel/plugin-syntax-jsx',
+        'transform-vue-jsx'
+      ]
     }
   }
 }
